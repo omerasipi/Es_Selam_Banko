18157c3cafbb51cb985b44e5b2716da9ea56cb48
asipiome <asipiome@students.zhaw.ch>
2024-10-30 22:48:46 +0100

    Initialize Spring Boot project with Gradle wrapper
    Set up a new Spring Boot project structure with a `bankParser` application. Added Gradle wrapper scripts, build configuration, project settings, and necessary Spring Boot dependencies.

---
+++ src/main/java/ch/asipiit/bankparser/BankParserApplication.java
@@ -0,0 +1,13 @@
+package ch.asipiit.bankparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/asipiit/bankparser/BankParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.asipiit.bankparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
